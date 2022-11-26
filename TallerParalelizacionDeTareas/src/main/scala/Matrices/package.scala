79a85498e6238e252d968de8c0299b88bc93bd6d
GetialJuan <getial.juan@correounivalle.edu.co>
2022-11-26 12:26:20 -0500

    codigo terminado

--- TallerParalelizacionDeTareas/src/main/scala/Matrices/package.scala
+++ TallerParalelizacionDeTareas/src/main/scala/Matrices/package.scala
@@ -97,8 +97,27 @@ package object Matrices {
     1.2.4. Multiplicando matrices recursivamente, version paralela
   */
   def multMatrizRecPar(m1:Matriz, m2:Matriz):Matriz = {
-    //Por terminar....
-    Vector(Vector(1))
+    def unirMatriz(m1: Matriz, m2: Matriz): Matriz = {
+      val l = m1.length
+      Vector.tabulate(l)((i) => m1(i) ++ m2(i))
+    }
+
+    val l = m1.length
+    if (l == 1) Vector(Vector(m1(0)(0) * m2(0)(0)))
+    else {
+      val multsLeft = task(for (i <- (0 to 1).toVector; j <- (0 to 1).toVector) yield
+        multMatrizRecPar(subMatriz(m1, l * i / 2, 0, l / 2), subMatriz(m2, 0, l * j / 2, l / 2)))
+      val multsRight = task(for (i <- (0 to 1).toVector; j <- (0 to 1).toVector) yield
+        multMatrizRecPar(subMatriz(m1, l * i / 2, l / 2, l / 2), subMatriz(m2, l / 2, l * j / 2, l / 2)))
+
+      val multsL = multsLeft.join()
+      val multsR = multsRight.join()
+
+      val subMTotales = for (i <- (0 to 3).toVector) yield sumMatriz(multsL(i), multsR(i))
+
+      unirMatriz(subMTotales(0), subMTotales(1)) ++ unirMatriz(subMTotales(2), subMTotales(3))
+
+    }
   }
 
   /*
@@ -107,5 +126,102 @@ package object Matrices {
   def restaMatriz(m1:Matriz, m2:Matriz):Matriz = {
     val l = m1.length
     Vector.tabulate(l, l)((i, j) => m1(i)(j) - m2(i)(j))
+  }
+
+  /*
+    1.3.2. Algoritmo de Strassen, version secuencial
+  */
+  def multStrassen(m1:Matriz, m2:Matriz):Matriz = { //m1 = A, m2, = B
+    def unirMatriz(m1: Matriz, m2: Matriz): Matriz = {
+      val l = m1.length
+      Vector.tabulate(l)((i) => m1(i) ++ m2(i))
+    }
+    val l = m1.length
+    if (l < 2) {
+      Vector(Vector( m1(0)(0)*m2(0)(0) ))
+    }
+    else {
+      val subMatricesM1 = for (i <- (0 to 1).toVector; j <- (0 to 1).toVector) yield
+        subMatriz(m1, l * i / 2, l * j / 2, l / 2) // [A11, A12, A21, A22]
+      val subMatricesM2 = for (i <- (0 to 1).toVector; j <- (0 to 1).toVector) yield
+        subMatriz(m2, l * i / 2, l * j / 2, l / 2) // [B11, B12, B21, B22]
+
+      val s1 = restaMatriz(subMatricesM2(1), subMatricesM2(3)) //B12 - B22
+      val s2 = sumMatriz(subMatricesM1(0), subMatricesM1(1)) //A11 + A12
+      val s3 = sumMatriz(subMatricesM1(2), subMatricesM1(3)) //A21 + A22
+      val s4 = restaMatriz(subMatricesM2(2), subMatricesM2(0)) //B21 - B11
+      val s5 = sumMatriz(subMatricesM1(0), subMatricesM1(3)) //A11 + A22
+      val s6 = sumMatriz(subMatricesM2(0), subMatricesM2(3)) //B11 + B22
+      val s7 = restaMatriz(subMatricesM1(1), subMatricesM1(3)) //A12 - A22
+      val s8 = sumMatriz(subMatricesM2(2), subMatricesM2(3)) //B21 + B22
+      val s9 = restaMatriz(subMatricesM1(0), subMatricesM1(2)) //A11 - A21
+      val s10 = sumMatriz(subMatricesM2(0), subMatricesM2(1)) //B11 + B12
+
+      val p1 = multStrassen( subMatricesM1(0), s1 ) //A11*S1
+      val p2 = multStrassen( s2, subMatricesM2(3) ) //s2*B22
+      val p3 = multStrassen( s3, subMatricesM2(0) ) //s3*B11
+      val p4 = multStrassen( subMatricesM1(3), s4 ) //A22*S4
+      val p5 = multStrassen( s5, s6 ) //s5*s6
+      val p6 = multStrassen( s7, s8 ) //s7*S8
+      val p7 = multStrassen( s9, s10 ) //s9*s10
+
+      val c11 = sumMatriz(restaMatriz(sumMatriz(p5, p4), p2), p6)
+      val c12 = sumMatriz(p1, p2)
+      val c21 = sumMatriz(p3, p4)
+      val c22 = restaMatriz(restaMatriz(sumMatriz(p5, p1), p3), p7)
+
+      unirMatriz(c11, c12) ++ unirMatriz(c21, c22)
+    }
+
+  }
+
+  /*
+    1.3.3. Algoritmo de Strassen, version paralela
+  */
+  def multStrassenPar(m1: Matriz, m2: Matriz): Matriz = {
+    def unirMatriz(m1: Matriz, m2: Matriz): Matriz = {
+      val l = m1.length
+      Vector.tabulate(l)((i) => m1(i) ++ m2(i))
+    }
+
+    val l = m1.length
+    if (l < 2) {
+      Vector(Vector(m1(0)(0) * m2(0)(0)))
+    }
+    else {
+      val subMatricesM1 = task(for (i <- (0 to 1).toVector; j <- (0 to 1).toVector) yield
+        subMatriz(m1, l * i / 2, l * j / 2, l / 2)) // [A11, A12, A21, A22]
+      val subMatricesM2 = task(for (i <- (0 to 1).toVector; j <- (0 to 1).toVector) yield
+        subMatriz(m2, l * i / 2, l * j / 2, l / 2)) // [B11, B12, B21, B22]
+
+      val subMatrices1 = subMatricesM1.join()
+      val subMatrices2 = subMatricesM2.join()
+
+      val s1 = restaMatriz(subMatrices2(1), subMatrices2(3)) //B12 - B22
+      val s2 = sumMatriz(subMatrices1(0), subMatrices1(1)) //A11 + A12
+      val s3 = sumMatriz(subMatrices1(2), subMatrices1(3)) //A21 + A22
+      val s4 = restaMatriz(subMatrices2(2), subMatrices2(0)) //B21 - B11
+      val s5 = sumMatriz(subMatrices1(0), subMatrices1(3)) //A11 + A22
+      val s6 = sumMatriz(subMatrices2(0), subMatrices2(3)) //B11 + B22
+      val s7 = restaMatriz(subMatrices1(1), subMatrices1(3)) //A12 - A22
+      val s8 = sumMatriz(subMatrices2(2), subMatrices2(3)) //B21 + B22
+      val s9 = restaMatriz(subMatrices1(0), subMatrices1(2)) //A11 - A21
+      val s10 = sumMatriz(subMatrices2(0), subMatrices2(1)) //B11 + B12
+
+      val p1 = task(multStrassenPar(subMatrices1(0), s1)) //A11*S1
+      val p2 = task(multStrassenPar(s2, subMatrices2(3))) //s2*B22
+      val p3 = task(multStrassenPar(s3, subMatrices2(0))) //s3*B11
+      val p4 = task(multStrassenPar(subMatrices1(3), s4)) //A22*S4
+      val p5 = task(multStrassenPar(s5, s6)) //s5*s6
+      val p6 = task(multStrassenPar(s7, s8)) //s7*S8
+      val p7 = task(multStrassenPar(s9, s10)) //s9*s10
+
+      val c11 = sumMatriz(restaMatriz(sumMatriz(p5.join(), p4.join()), p2.join()), p6.join())
+      val c12 = sumMatriz(p1.join(), p2.join())
+      val c21 = sumMatriz(p3.join(), p4.join())
+      val c22 = restaMatriz(restaMatriz(sumMatriz(p5.join(), p1.join()), p3.join()), p7.join())
+
+      unirMatriz(c11, c12) ++ unirMatriz(c21, c22)
+    }
   }
 }
--- TallerParalelizacionDeTareas/src/test/scala/Pruebas.sc
+++ TallerParalelizacionDeTareas/src/test/scala/Pruebas.sc
@@ -1,11 +1,15 @@
 import Matrices._
-
+import Benchmark._
+/*
 val m1 = matrizAlAzar(1,2)
 val m2 = matrizAlAzar(2,2)
 val m3 = matrizAlAzar(4,2)
 val m4 = matrizAlAzar(4,2)
-
+*/
 //1.1.1. multMatriz
+
+//1.1.2. multMatrizPar
+//multMatrizPar(m3,m4)
 
 //1.2.1 subMatriz
 
@@ -14,6 +18,28 @@ val m4 = matrizAlAzar(4,2)
 //1.2.3 multMatrizRec
 
 //1.2.4 multMatrizRecPar
-//sin hacer
 
 //1.3.1 restaMatriz
+
+//1.3.2 multStrassen
+
+//1.3.3 multStrassenPar
+
+///////EVALUACION COMPARATIVA///////
+
+def evaluacionComparativa(f1:(Matriz,Matriz) => Matriz, f2:(Matriz,Matriz) => Matriz) = {
+  for (
+    i <- (1 to 10 );
+    m1 = matrizAlAzar(math.pow(2, i).toInt, 2);
+    m2 = matrizAlAzar(math.pow(2, i).toInt, 2)
+  ) yield (compararAlgoritmos(f1, f2)(m1, m2), math.pow(2, i).toInt)
+}
+
+//multMatriz vs multMatrizPar
+//evaluacionComparativa(multMatriz, multMatrizPar)
+
+//multMatrizRec vs multMatrizRecPar
+//evaluacionComparativa(multMatrizRec, multMatrizRecPar)
+
+//multStrassen vs multStrassenPar
+//evaluacionComparativa(multStrassen, multStrassenPar)
